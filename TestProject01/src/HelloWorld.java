31b6df87b249f50b2024d78c2b9f04c2684de55b
masa1000ten <masa1000ten@yahoo.co.jp>
2018-12-12 21:17:51 +0900

    12/12

--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -3,11 +3,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Hello World! の出力
-		int a = 5;
-		System.out.println(a++);
-		System.out.println(a++);
-		System.out.println(a++);
-		System.out.println(a++);
-	}
+		String a = "赤";
 
+		switch(a){
+		case "赤":
+			System.out.println("赤組です");
+			break;
+		case "白":
+			System.out.println("白組です");
+			break;
+		}
+	}
 }
